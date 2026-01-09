8fa13621eae7be1863cc1fc8451a2f85b8cfbf60
JuanECK <juan_eck@hotmail.com>
2026-01-09 02:00:02 -0600

    commmit

--- app/models/cla.php
+++ app/models/cla.php
@@ -22,9 +22,12 @@ class CLA {
         $datos = json_decode( $_POST['carga'], true );
         // echo json_encode($datos);
         $values = array_values( $datos[0] );
-        [ $codigo_empleado, $empleado, $estatus, $puesto, $departamento, $salario_dia, $sueldo_mensual, $RFC, $NSS, $fecha_ingreso, $fecha_reingreso, $fecha_baja, $domicilio, $codigo_pos, $estado, $fecha_nacimiento, $_decimal, $edad, $telefono, $sexo, $estado_civil, $correo ] = $values;
-        // [ $n1, $n2, $n3, $n4 ] = $values;
-        // return $datos;
+        [ $codigo_empleado, $empleado, $estatus, $puesto, $departamento, $salario_dia, $sueldo_mensual, $RFC, $NSS, $CURP, $fecha_ingreso, $fecha_reingreso, $fecha_baja, $domicilio, $codigo_pos, $estado, $fecha_nacimiento, $_decimal, $edad, $telefono, $sexo, $estado_civil, $correo ] = $values;
+
+         $sql = "CALL `inserta_empleado1`(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        $stmt = $this -> conn -> prepare( $sql );
+        $stmt->execute(array($codigo_empleado, $empleado, $estatus, $puesto, $departamento, $salario_dia, $sueldo_mensual, $RFC, $NSS, $CURP, $fecha_ingreso, $fecha_reingreso, $fecha_baja, $domicilio, $codigo_pos, $estado, $fecha_nacimiento, $_decimal, $edad, $telefono, $sexo, $estado_civil, $correo));
+        // return 'exitoso';
         return $values;
     }
 }
