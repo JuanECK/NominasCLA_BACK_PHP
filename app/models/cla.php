da09aa1c99eafa8c6eb73d8d35bd84b4b7843403
JuanECK <juan_eck@hotmail.com>
2026-01-15 01:00:02 -0600

    revision y cambios

--- app/models/cla.php
+++ app/models/cla.php
@@ -20,16 +20,27 @@ class CLA {
         die(json_encode("Error: La conexión no se ha inicializado correctamente."));
         }
 
-        $datos = json_decode( $_POST['carga'], true );
-        // echo json_encode($datos);
-        $values = array_values( $datos[0] );
-        [ $codigo_empleado, $empleado, $estatus, $puesto, $departamento, $salario_dia, $sueldo_mensual, $RFC, $NSS, $CURP, $fecha_ingreso, $fecha_reingreso, $fecha_baja, $domicilio, $codigo_pos, $estado, $fecha_nacimiento, $_decimal, $edad, $telefono, $sexo, $estado_civil, $correo ] = $values;
+        try{
 
-         $sql = "CALL `inserta_empleado1`(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-        $stmt = $this -> conn -> prepare( $sql );
-        $stmt->execute(array($codigo_empleado, $empleado, $estatus, $puesto, $departamento, $salario_dia, $sueldo_mensual, $RFC, $NSS, $CURP, $fecha_ingreso, $fecha_reingreso, $fecha_baja, $domicilio, $codigo_pos, $estado, $fecha_nacimiento, $_decimal, $edad, $telefono, $sexo, $estado_civil, $correo));
-        // return 'exitoso';
-        return $values;
+            $datos = json_decode( $_POST['carga'], true );
+            // echo json_encode($datos);
+            $values = array_values( $datos[0] );
+            [ $codigo_empleado, $empleado, $estatus, $puesto, $departamento, $salario_dia, $sueldo_mensual, $RFC, $NSS, $CURP, $fecha_ingreso, $fecha_reingreso, $fecha_baja, $domicilio, $codigo_pos, $estado, $fecha_nacimiento, $_decimal, $edad, $telefono, $sexo, $estado_civil, $correo ] = $values;
+    
+             $sql = "CALL `inserta_empleado1`(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+            $stmt = $this -> conn -> prepare( $sql );
+            $stmt->execute(array($codigo_empleado, $empleado, $estatus, $puesto, $departamento, $salario_dia, $sueldo_mensual, $RFC, $NSS, $CURP, $fecha_ingreso, $fecha_reingreso, $fecha_baja, $domicilio, $codigo_pos, $estado, $fecha_nacimiento, $_decimal, $edad, $telefono, $sexo, $estado_civil, $correo));
+            // return 'exitoso';
+            return $values;
+        } catch(\PDOException $e) {
+    // ESTO MOSTRARÁ EL ERROR REAL DE MYSQL
+    echo "<h3>Error de Base de Datos:</h3>";
+    echo "Mensaje: " . $e->getMessage() . "<br>";
+    echo "Código de error: " . $e->getCode();
+    
+    // Si quieres ver si el problema es el número de parámetros:
+    // echo "Parámetros enviados: " . count($parametros);
+}
     }
 
     public function SetInsertaPropiedades () {
@@ -41,7 +52,7 @@ class CLA {
         $datos = json_decode($_POST['carga'],true);
         $values = array_values( $datos[0] );
         [ $empleado_id, $indemnizacion_90_dias, $indemnizacion_20_dias, $prima_antiguedad, $prestacion_vacaciones, $prestacion_pv, $prestacion_aguinaldo, $dias_año, $salario_diario, $salario_diario_integrado, $uma, $salario_minimo_general, $dias_a_pagar, $horas_turno, $faltas_periodo, $incapacidades, $dias_vales_despensa_efectivo, $dias_vales_despensa_especie, $umi, $septimo_dia, $infonavit_porcent, $infonavit_FD, $infonavit_CF, $dias_bimestre, $dias_infonavit, $vales_despensa_QNA, $vales_despensa_SEM, $fondo_ahorro_QNA, $fondo_ahorro_SEM, $cuota_sindical, $pension_alimanticia, $bono_puntualidad, $vacaciones_pendientes, $acum_ausentismos, $acum_incapacidades, $acum_infonavit, $acum_seguro_daños_info, $acum_fondo_ahorro, $acum_exento_prima_vac, $acum_exento_aguinaldo, $base_gravable_pendiente, $aguinaldo_SDI, $prima_vacacional_SDI, $variable_SDI, $fondo_ahorro_SDI, $despensa_SDI, $gratificación ] = $values;
-        $sql = "CALL `inserta_empleado1`(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+        $sql = "CALL `inserta_propiedades`(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
         $stmt = $this -> conn -> prepare( $sql );
         $stmt -> execute( array($empleado_id, $indemnizacion_90_dias, $indemnizacion_20_dias, $prima_antiguedad, $prestacion_vacaciones, $prestacion_pv, $prestacion_aguinaldo, $dias_año, $salario_diario, $salario_diario_integrado, $uma, $salario_minimo_general, $dias_a_pagar, $horas_turno, $faltas_periodo, $incapacidades, $dias_vales_despensa_efectivo, $dias_vales_despensa_especie, $umi, $septimo_dia, $infonavit_porcent, $infonavit_FD, $infonavit_CF, $dias_bimestre, $dias_infonavit, $vales_despensa_QNA, $vales_despensa_SEM, $fondo_ahorro_QNA, $fondo_ahorro_SEM, $cuota_sindical, $pension_alimanticia, $bono_puntualidad, $vacaciones_pendientes, $acum_ausentismos, $acum_incapacidades, $acum_infonavit, $acum_seguro_daños_info, $acum_fondo_ahorro, $acum_exento_prima_vac, $acum_exento_aguinaldo, $base_gravable_pendiente, $aguinaldo_SDI, $prima_vacacional_SDI, $variable_SDI, $fondo_ahorro_SDI, $despensa_SDI, $gratificación) );
 
